f317c10edd563b9eaced1bc679b7f1479abfa123
ugaleankita <ugaleankita582@gmail.com>
2024-09-12 15:17:04 +0530

    first commit

---
+++ 1program javascript.js
@@ -0,0 +1,80 @@
+// var addition;
+//   var num1=parseInt(window.prompt(enter any no:));
+//   var num2=parseInt(window.prompt(enter any no:));
+//   var operation=parseInt(window.prompt(enter any no:));
+//   switch (operation){
+//     case "+": num1+num2;
+//               console.log(num1+num2=num3)
+//               break;
+//   }
+// switch(num){
+//   case 1: var number1 = (prompt('Enter first number: '));
+//             var number2 =(prompt('Enter second number: '));
+
+//      console.log( num1+num2)
+//      break;
+// }
+
+
+// // program for a simple calculator
+// let result;
+
+// // take the operator input
+// const operator = prompt('Enter operator ( either +, -, * or / ): ');
+
+// // take the operand input
+// const number1 = parseFloat(prompt('Enter first number: '));
+// const number2 = parseFloat(prompt('Enter second number: '));
+
+// switch(operator) {
+//     case '+':
+//          result = number1 + number2;
+//         // console.log(`${number1} + ${number2} = ${result}`);
+
+//         break;
+
+//     case '-':
+//          result = number1 - number2;
+//         console.log(`${number1} - ${number2} = ${result}`);
+//         break;
+
+//     case '*':
+//          result = number1 * number2;
+//         console.log(`${number1} * ${number2} = ${result}`);
+//         break;
+
+//     case '/':
+//          result = number1 / number2;
+//         console.log(`${number1} / ${number2} = ${result}`);
+//         break;
+
+//     default:
+//         console.log('Invalid operator');
+//         break;
+// }
+  
+// const operator = prompt('Enter operator ( either +, -, * or / ): ');
+
+// // take the operand input
+// const number1 = parseFloat(prompt('Enter first number: '));
+// const number2 = parseFloat(prompt('Enter second number: '));
+
+// let result;
+
+// // using if...else if... else
+// if (operator == '+') {
+//     result = number1 + number2;
+// }
+// else if (operator == '-') {
+//     result = number1 - number2;
+// }
+// else if (operator == '*') {
+//     result = number1 * number2;
+// }
+// else {
+//     result = number1 / number2;
+// }
+
+// // display the result
+// console.log(${number1} ${operator} ${number2} = ${result});
+  
